6910bc4890d12329693f2c5cf88cc62fe36a1256
evgenijerm <erm.evgeniy18@gmail.com>
2020-06-17 11:46:32 +0300

    CallTest

--- src/test/java/CallTest.java
+++ src/test/java/CallTest.java
@@ -1,2 +1,22 @@
-public class CallTest {
-}
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Selenide.*;
+
+        public class CallTest {
+
+            @Test
+            void shouldTestV1() {
+                open("http://localhost:9999");
+                $(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[2]/input")).sendKeys("Василий Пупкин");
+                $("[data-test-id=phone] input").sendKeys("+79270000000");
+                $("[data-test-id=agreement]").click();
+                $("button.button").click();
+                String actual = $("[data-test-id=order-success]").getText();
+                assertEquals("Ваша заявка успешно отправлена! ", actual.trim());
+
+            }
+        }
